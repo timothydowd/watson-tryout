2c88cfedb2927efd79c9d51de9809ac70ce289ef
timothydowd <timdowd1984@gmail.com>
2019-03-03 18:35:24 +0000

    incorporated promise.all to pass multiple datas down then chain to views

--- controllers/speechTextToneController.js
+++ controllers/speechTextToneController.js
@@ -1,6 +1,6 @@
 const speechToText = require('../models/speechToTextModel')
 const readTone = require('../models/readToneModel')
-
+/*
 const speechConvert = (req, res, next) => {
   
     speechToText()
@@ -16,12 +16,28 @@ const speechConvert = (req, res, next) => {
         res.render('pages/home', { toneAnalysis })
     })
     .catch((err) => console.log('error:', err) || next ({err}))
+}
+*/
 
-
+const speechConvert = (req, res, next) => {
+    speechToText()
+    .then(resultText => Promise.all([readTone(resultText.results[0].alternatives[0].transcript), resultText]))
+    .then(([toneAnalysis, resultText]) => {  
+        res.render('pages/home', { toneAnalysis, resultText })
+    })
+    .catch((err) => console.log('error:', err) || next ({err}))
 }
 
-
-
+/*
+exports.getSongAnalysis = (req, res, next) => {
+    const { artist, track } = req.query;
+    getLyrics(artist, track)
+      .then(lyrics => Promise.all([analyseTone(lyrics), lyrics]))
+      .then(([tones, lyrics]) =>
+        res.render('tone', { tones, lyrics, track, artist }),
+      );
+  };
+*/
 
 
 module.exports = speechConvert;
